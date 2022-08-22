6e7e435d24c8167b3a22dd632402c187b9f5f837
季洪俊 <jihongjun@58.com>
2022-08-23 01:00:30 +0800

    修改重启平台脚本的bug

--- bin/run.sh
+++ bin/run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-pid=$(ps -ef | grep python3 | grep manage | awk '{print $2}')
-echo $pid
-if [[$pid]]; then
+# shellcheck disable=SC2009
+pid=$(ps -ef | grep python3 | grep "manage.py runserver 0.0.0.0:8001" | awk '{print $2}')
+echo "$pid"
+if [ "$pid" ]; then
   kill -9 $pid
-else
+pid=$(ps -ef | grep python3 | grep "manage.py runserver 0.0.0.0:8001" | awk '{print $2}')
+echo "$pid"
+if [ ! $pid ]; then
   echo "#####Django服务成功关闭#####"
   echo "#####Django启动服务#####"
-  nohup python3 $(dirname `readlink -f $0`)/../manage.py runserver 0.0.0.0:8000 --noreload &
-  pid=$(ps -ef | grep python3 | grep manage | awk '{print $2}' | wc -l)
-  echo $pid
-  if [ ! $pid ]; then
-    echo "#####Django服务启动失败#####"
-  else
-    echo "#####Django服务启动成功#####"
-  fi
+  nohup python3 $(pwd)/manage.py runserver 0.0.0.0:8001 &
+pid=$(ps -ef | grep python3 | grep "manage.py runserver 0.0.0.0:8001" | awk '{print $2}')
+echo "$pid"
+if [ ! $pid ]; then
+  echo "#####Django服务启动失败#####"
+else
+  echo "#####Django服务启动成功#####"
+fi
+fi
 fi
