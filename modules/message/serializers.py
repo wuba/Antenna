fdcd62297494bdceeddf290903565a33868e0700
bios000 <3540970872@qq.com>
2023-03-20 11:39:02 +0800

    优化api查询消息接口

--- modules/message/serializers.py
+++ modules/message/serializers.py
@@ -18,7 +18,12 @@ class MessageFilter(django_filters.FilterSet):
     content_contains = django_filters.CharFilter(field_name='content', lookup_expr='icontains')
     domain_contains = django_filters.CharFilter(field_name='domain', lookup_expr='icontains')
     order_desc = django_filters.CharFilter(method='order_by_desc', initial='desc')
-
+    task_name = django_filters.CharFilter(field_name='task__name')
+    message_type = django_filters.NumberFilter(field_name='message_type')
+    template_name = django_filters.CharFilter(field_name='template__name')
+    task = django_filters.NumberFilter(field_name='task')
+    content = django_filters.CharFilter(field_name='content')
+    create_time = django_filters.DateFilter(field_name="create_time")
     def order_by_desc(self, queryset, name, value):
         if value == "asc":
             return queryset.order_by('create_time')
@@ -27,5 +32,3 @@ class MessageFilter(django_filters.FilterSet):
     class Meta:
         model = Message
         fields = "__all__"
-
-
