02a97fc3088d04c94963da28d40b3c3cb6d1d86e
bios000 <3540970872@qq.com>
2023-03-20 11:05:50 +0800

    优化api查询消息接口

--- modules/message/serializers.py
+++ modules/message/serializers.py
@@ -12,20 +12,20 @@ class MessageSerializer(serializers.ModelSerializer):
 
 
 class MessageFilter(django_filters.FilterSet):
-    class MessageFilter(django_filters.FilterSet):
-        month = django_filters.NumberFilter(field_name='create_time', lookup_expr='month')
-        year = django_filters.NumberFilter(field_name='create_time', lookup_expr='year')
-        domain_in = django_filters.CharFilter(field_name='domain', lookup_expr='in')
-        content_contains = django_filters.CharFilter(field_name='content', lookup_expr='icontains')
-        domain_contains = django_filters.CharFilter(field_name='domain', lookup_expr='icontains')
-        order_desc = django_filters.CharFilter(method='order_by_desc', initial='desc')
+    month = django_filters.NumberFilter(field_name='create_time', lookup_expr='month')
+    year = django_filters.NumberFilter(field_name='create_time', lookup_expr='year')
+    domain_in = django_filters.CharFilter(field_name='domain', lookup_expr='in')
+    content_contains = django_filters.CharFilter(field_name='content', lookup_expr='icontains')
+    domain_contains = django_filters.CharFilter(field_name='domain', lookup_expr='icontains')
+    order_desc = django_filters.CharFilter(method='order_by_desc', initial='desc')
 
-        class Meta:
-            model = Message
-            fields = "__all__"
+    def order_by_desc(self, queryset, name, value):
+        if value == "asc":
+            return queryset.order_by('create_time')
+        return queryset.order_by('-create_time')
 
-        def order_by_desc(self, queryset, name, value):
-            if value == "asc":
-                return queryset.order_by('create_time')
-            else:
-                return queryset.order_by('-create_time')
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
