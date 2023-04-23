0b034102f40ff2d3707d8c1f82e15a6134702842
bios000 <3540970872@qq.com>
2023-04-23 10:56:52 +0800

    优化https 查询场景

--- modules/template/depend/listen/httpslog.py
+++ modules/template/depend/listen/httpslog.py
@@ -13,11 +13,14 @@ from io import BytesIO
 from twisted.web.client import FileBodyProducer
 from twisted.internet.defer import succeed
 
+
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__) + "../../../../../")
 sys.path.append(PROJECT_ROOT)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'antenna.settings'
 
 django.setup()
+from modules.config import setting
 from modules.template.depend.base import BaseTemplate
 from modules.message.constants import MESSAGE_TYPES
 from modules.message.models import Message
@@ -57,27 +60,28 @@ class MyResource(Resource):
                 headers += f"{name.decode('utf-8')}: {value.decode('utf-8')}\r\n"
 
         self.html = f"{request_line}\r\n{headers}\r\n{self.body}"
-        print(f"Full HTTP request:\n{self.html}")
 
         parsed_uri = urlparse(request.uri.decode('utf-8'))
         query_params = parse_qs(parsed_uri.query)
         if 'message' in query_params:
             self.content = query_params['message'][0]
 
-        task_config_item = TaskConfigItem.objects.filter(task_config__key=self.key, task__status=1).first()
-        if task_config_item and task_config_item.template.name == "HTTPS":
-            username = task_config_item.task.user.username
-            send_email_message(username, self.remote_addr)
-            Message.objects.create(domain=self.domain, message_type=MESSAGE_TYPES.HTTPS,
-                                   remote_addr=self.remote_addr,
-                                   task_id=task_config_item.task_id,
-                                   uri=self.uri,
-                                   template_id=task_config_item.template_id,
-                                   content=self.content,
-                                   html=self.html)
-            send_message(url=self.domain, remote_addr=self.remote_addr, uri=self.uri, header='',
-                         message_type=MESSAGE_TYPES.HTTPS, content=self.content,
-                         task_id=task_config_item.task_id, raw=self.html)
+        if len(self.key) == 4 and self.key != setting.PLATFORM_DOMAIN.split('.')[0]:
+            task_config_item = TaskConfigItem.objects.filter(task_config__key__iexact=self.key,
+                                                             task__status=1).first()
+            if task_config_item and task_config_item.template.name == "HTTPS":
+                username = task_config_item.task.user.username
+                send_email_message(username, self.remote_addr)
+                Message.objects.create(domain=self.domain, message_type=MESSAGE_TYPES.HTTPS,
+                                       remote_addr=self.remote_addr,
+                                       task_id=task_config_item.task_id,
+                                       uri=self.uri,
+                                       template_id=task_config_item.template_id,
+                                       content=self.content,
+                                       html=self.html)
+                send_message(url=self.domain, remote_addr=self.remote_addr, uri=self.uri, header='',
+                             message_type=MESSAGE_TYPES.HTTPS, content=self.content,
+                             task_id=task_config_item.task_id, raw=self.html)
 
         agent = Agent(reactor, pool=self.pool)
         body_producer = FileBodyProducer(request.content)
