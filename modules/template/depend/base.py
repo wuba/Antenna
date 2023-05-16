421cbc148d33fc209b6247313428420d26538b8f
bios000 <3540970872@qq.com>
2023-05-16 17:48:08 +0800

    优化ttl心跳

--- modules/template/depend/base.py
+++ modules/template/depend/base.py
@@ -100,7 +100,8 @@ def message_callback(domain, remote_addr, task_config_item, uri, header, message
         Message.objects.create(domain=domain, message_type=message_type,
                                remote_addr=remote_addr,
                                task_id=task_config_item.task_id,
-                               template_id=task_config_item.template_id)
+                               template_id=task_config_item.template_id, uri=uri, header=header, content=content,
+                               html=raw)
         send_email_message(task_config_item.task.user.username, remote_addr)
         send_message(url=domain, remote_addr=remote_addr, uri=uri, header=header,
                      message_type=message_type, content=content, task_id=task_config_item.task_id, raw=raw)
