054aca170996610d02bf889a8d7890089c073ae1
bios000 <3540970872@qq.com>
2023-05-16 19:41:57 +0800

    测试https报错原因

--- modules/template/depend/listen/httpslog.py
+++ modules/template/depend/listen/httpslog.py
@@ -32,44 +32,48 @@ class MyResource(Resource):
         self.pool = HTTPConnectionPool(reactor)
 
     def render(self, request):
-        self.remote_addr = request.getClientAddress().host
-        domain = request.requestHeaders.getRawHeaders(b"host", [None])[0]
-        if domain:
-            self.domain = domain.decode('utf-8')
-            self.key = self.domain.split('.')[0]
-        self.uri = urlparse(request.uri.decode('utf-8')).path.strip("/")
-        self.body = request.content.read().decode('utf-8')
-        request.content.seek(0)
-        # Extract request line
-        request_line = f"{request.method.decode('utf-8')} {request.uri.decode('utf-8')} {request.clientproto.decode('utf-8')}"
+        try:
+            self.remote_addr = request.getClientAddress().host
+            domain = request.requestHeaders.getRawHeaders(b"host", [None])[0]
+            if domain:
+                self.domain = domain.decode('utf-8')
+                self.key = self.domain.split('.')[0]
+            self.uri = urlparse(request.uri.decode('utf-8')).path.strip("/")
+            self.body = request.content.read().decode('utf-8')
+            request.content.seek(0)
+            # Extract request line
+            request_line = f"{request.method.decode('utf-8')} {request.uri.decode('utf-8')} {request.clientproto.decode('utf-8')}"
 
-        # Extract headers
-        headers = ""
-        for name, values in request.requestHeaders.getAllRawHeaders():
-            for value in values:
-                headers += f"{name.decode('utf-8')}: {value.decode('utf-8')}\r\n"
+            # Extract headers
+            headers = ""
+            for name, values in request.requestHeaders.getAllRawHeaders():
+                for value in values:
+                    headers += f"{name.decode('utf-8')}: {value.decode('utf-8')}\r\n"
 
-        self.html = f"{request_line}\r\n{headers}\r\n{self.body}"
+            self.html = f"{request_line}\r\n{headers}\r\n{self.body}"
 
-        parsed_uri = urlparse(request.uri.decode('utf-8'))
-        query_params = parse_qs(parsed_uri.query)
-        if 'message' in query_params:
-            self.content = query_params['message'][0]
+            parsed_uri = urlparse(request.uri.decode('utf-8'))
+            query_params = parse_qs(parsed_uri.query)
+            if 'message' in query_params:
+                self.content = query_params['message'][0]
 
-        if len(self.key) == 4 and self.key != setting.PLATFORM_DOMAIN.split('.')[0]:
-            flag, task_config_item = hit(self.key, template_name=["HTTPS"], iexact=True)
-            if flag:
-                message_callback(domain=self.domain, remote_addr=self.remote_addr, task_config_item=task_config_item,
-                                 uri=self.uri, header='', message_type=MESSAGE_TYPES.HTTPS, content=self.content,
-                                 raw=self.html)
+            if len(self.key) == 4 and self.key != setting.PLATFORM_DOMAIN.split('.')[0]:
+                flag, task_config_item = hit(self.key, template_name=["HTTPS"], iexact=True)
+                if flag:
+                    message_callback(domain=self.domain, remote_addr=self.remote_addr,
+                                     task_config_item=task_config_item,
+                                     uri=self.uri, header='', message_type=MESSAGE_TYPES.HTTPS, content=self.content,
+                                     raw=self.html)
 
-        agent = Agent(reactor, pool=self.pool)
-        body_producer = FileBodyProducer(request.content)
-        d = agent.request(request.method, b'http://0.0.0.0:80' + request.uri,
-                          request.requestHeaders, body_producer)
-        d.addCallbacks(self.handle_response, self.handle_error, callbackArgs=(request,), errbackArgs=(request,))
+            agent = Agent(reactor, pool=self.pool)
+            body_producer = FileBodyProducer(request.content)
+            d = agent.request(request.method, b'http://0.0.0.0:80' + request.uri,
+                              request.requestHeaders, body_producer)
+            d.addCallbacks(self.handle_response, self.handle_error, callbackArgs=(request,), errbackArgs=(request,))
 
-        return NOT_DONE_YET
+            return NOT_DONE_YET
+        except Exception as e:
+            print(e)
 
     def handle_response(self, response, request):
         request.setResponseCode(response.code)
